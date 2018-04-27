07508969445dcca337514800538ce38082d0f679
U185498 <kejsi.struga.sp@lhind.dlh.de>
2018-04-27 12:05:15 +0200

    Debugging, app error

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,10 @@
-module.exports = {
-	mongoURI: process.env.MONGO_URI,
-	googleClientID: process.env.GOOGLE_CLIENT_ID,
-	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
-}
+mongoURI=process.env.MONGO_URI,
+googleClientID=process.env.GOOGLE_CLIENT_ID,
+googleClientSecret=process.env.GOOGLE_CLIENT_SECRET
+
+console.log("Debugging keys_prod, error while loging in!");
+console.log(mongoURI)
+console.log(googleClientID)
+console.log(googleClientSecret)
+
+module.exports = mongoURI, googleClientID, googleClientSecret
